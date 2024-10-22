463d2d27878b443f56b3bd7624b18fbb766bb5f1
Lebid_Dmytro <lebedd99@gmail.com>
2024-10-22 16:46:35 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -8,11 +8,16 @@ from .views import (
     CarUpdateView,
     CarDeleteView,
     DriverListView,
+    DriverCreateView,
+    DriverDeleteView,
     DriverDetailView,
+    DriverLicenseUpdateForm,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    AssignMeToCarView,
+    RemoveMeFromCarView,
 )
 
 urlpatterns = [
@@ -42,9 +47,30 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path(
+        "cars/<int:pk>/assign-me",
+        AssignMeToCarView.as_view(),
+        name="assign-me-to-car"
+    ),
+    path(
+        "cars/<int:pk>/remove-me",
+        RemoveMeFromCarView.as_view(),
+        name="remove-me-from-car"
+    ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverLicenseUpdateForm.as_view(),
+        name="driver-update"
     ),
 ]
 
